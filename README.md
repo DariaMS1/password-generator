99a464d9b0ca5ff695c25adc67eb390247c6e822
daria <darias2002@gmail.com>
2024-03-15 00:51:04 +0000

    code and readme update

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# password-generator
+
 Code to randomly generate a password according to user input.
+
+
+
+# password-generator
+
+## Description
+
+I built the project to gain a better understanding of JavaScript and nesting.
+
+This app helps the user generate secure passwords.
+
+I had many issues with this code, as I tried to create it without using the starter code (I kept it inside but commented out). Originally I wanted the user to be prompted on page load, instead of on button click; this created many scoping issues as I have nested the prompting functions inside conditional statements. The password variable's redefined value, from user input and randomised generation, made the variable's new value inaccessible, giving errors. Similarly to lengthInputIndex.
+
+I have resorted to the user prompts being activated on button click, which removed the previous scoping issues.
+
+I have added a <p> tag in the HTML to let the user know how to start the generation process.
+
+Overall I learned about the limitation of scoping and difficulty in function nesting.
+
+## Installation
+
+You could either clone the repo on your local machine using "git clone <repo-link>" inside the terminal or access it on the deployed page. 
+
+
+
+## Usage
+
+Click on the 'Generate Button' to begin prompting.
+
+![alt text](/challenge/assets/images/Screenshot%202024-03-15%20003544.png)
+
+## Credits
+
+https://github.com/DariaMS1 
+
+## License
+
+MIT License
+
+Copyright (c) [2024] [Daria Sora]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
--- challenge/assets/js/script.js
+++ challenge/assets/js/script.js
@@ -88,118 +88,80 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-//NO PROMPT ATTEMPT \/
-//                  \/
-//                  \/
-var allArrays = [         //mix all arrays to randomise
-  lowerCasedCharacters,   //only if all options accepted
-  upperCasedCharacters, 
-  numericCharacters, 
-  specialCharacters
-];
+// //NO PROMPT ATTEMPT \/
+// //                  \/
+// //                  \/
+// allArrays never gets used in the end //
+var allArrays = [lowerCasedCharacters, upperCasedCharacters, numericCharacters, specialCharacters];
 
 var inputArray = [];
 
-var generateBtn = document.querySelector('#generate');
+// had to 'outscope' the password as my previous attempts I tried to separate the generatePassword function from the parent optionsAll //
+// this created scoping issues where the password variable call did not use the redifined option (since it was nested inside optionsAll func) //
+var password = "";
 
-//dropping all options into one function. The code can't work if user cancels or puts wrong starting input.
 function optionsAll() {
-  //determining password length
-  var lengthInput = prompt("Enter a number between 8 and 128 for your password length.");
-  //instead of converting the prompt directly into an integer, I made two different variables.
-  //this is so if the user clicks 'cancel' on the prompt, it stops without following the conditional statements after.
-  var lengthInputIndex = parseInt(lengthInput);
-  //checking for all fault options.
-  function checkNaN() {
-    //differences between 'lengthInput' and 'lengthInputIndex' make it cleaner.
-    //the order of the conditional statement first checks for the input to be a number BEFORE checking if that number is between 8 and 128.
-    //'if (!lengthInput)' checks if user clicked cancel; ! notation means false 'lengthInput', i.e. cancel option.
-    //1st 'else if' statement checks for non numeric error.
-    //turned the string prompt into a number with 'parseInt'.
-    //'isNaN' means 'it's not a number'. Condition checks the 'lengthInputIndex' to be a number.
-    //alerts if there's an error.
-    //'optionsAll()' returns to initial function.
+    var lengthInput = prompt("Enter a number between 8 and 128 for your password length.");
+    var lengthInputIndex = parseInt(lengthInput);
     
-    //2nd 'else if' condition checks the right index input.
-    //condition is checked to meet requirements; then reversed (! notation),
-    //alerts the user of unmet requirements.
-    //'optionsAll()' restarts the process until user clicks cancel.
+    // check user input is appropriate //
     if (!lengthInput) {
-      return;
+        return;
     } else if (isNaN(lengthInputIndex)) {
-      alert("Use only numeric characters.")
-      optionsAll();
+        alert("Use only numeric characters.")
+        optionsAll();
     } else if (!(8 <= lengthInputIndex && lengthInputIndex <= 128)) {
-      alert("The number must be between 8 and 128.");
-      optionsAll();
+        alert("The number must be between 8 and 128.");
+        optionsAll();
     } else {
-      console.log(lengthInputIndex);
-      //you are calling this function here so it doesn't need calling at the end.
-      //'restOptions' function is tied to function 'checkNan', otherwise you can't continue.
-      function restOptions() {
-        
-        var lowerCaseAlert = confirm("Would you like to include lower case characters?");
-        console.log(`Lower cased characters: ` + lowerCaseAlert);
 
-          if (lowerCaseAlert) {
+        // user choice of password characters //
+        var lowerCaseAlert = confirm("Would you like to include lower case characters?");
+        if (lowerCaseAlert) {
             inputArray.push(lowerCasedCharacters);
-          };
+        };
 
         var upperCaseAlert = confirm("Would you like to include upper case characters?");
-        console.log(`Upper cased characters: ` + upperCaseAlert);
-
-          if (upperCaseAlert) {
+        if (upperCaseAlert) {
             inputArray.push(upperCasedCharacters);
-          };
+        };
 
         var numbersAlert = confirm("Would you like to include numbers?");
-        console.log(`Numeric characters: ` + numbersAlert);
-
-          if (numbersAlert) {
+        if (numbersAlert) {
             inputArray.push(numericCharacters);
-          };
+        };
 
-        var specialCharAlert = confirm("Would you like to inlclude special characters?");
-        console.log(`Special characters: ` + specialCharAlert);
-
-          if (specialCharAlert) {
+        var specialCharAlert = confirm("Would you like to include special characters?");
+        if (specialCharAlert) {
             inputArray.push(specialCharacters);
-          };
-      };
-    //calling the nested fuction inside 'else' statements.
-    restOptions();
-    //console logging the options neatly for easy view.
-    //logged twice to see the difference between.
-    console.log(`Characters available: ` + inputArray);
-    console.log(inputArray);
-    }
-  };
-  //call on the 'checkNaN' function to apply it.
-  checkNaN();
+        };
+
+        // generate password right after options end //
+        function generatePassword(lengthInputIndex, inputArray, password) {
+          var loopNr = lengthInputIndex;
+                  
+          for (var i = 0; i < loopNr; i++) {
+            var randomArrayIndex = Math.floor(Math.random() * inputArray.length);
+            var randomArray = inputArray[randomArrayIndex];
+            var randomCharIndex = Math.floor(Math.random() * randomArray.length);
+            password += randomArray[randomCharIndex];
+            }
+
+          console.log(password);
+          var passwordPrint = document.querySelector('#password');
+          passwordPrint.textContent = password;
+        }
+        // call the function inside optionsAll, so it generates after button click //
+        generatePassword(lengthInputIndex, inputArray, password);
+      }
 };
 
-// function generatePassword("click", )
+var generateBtn = document.querySelector('#generate');
 
-//only this function needs calling as 'restOptions' function is called inside of it, and is dependant on it.
-optionsAll();
-
-
-var password = [];
-
-function generatePassword() {
-  for (var i = 0; i < inputArray.length; i++) {
-    var randomChar = Math.floor(Math.random() * 8);
-    password.push(randomChar);
-  }
-  
-};
-
-console.log(password);
-
-var generateBtn = document.querySelector(`#generate`);
-var passwordPrint = document.querySelector(`#password`);
-
-generateBtn.addEventListener(`click`, generatePassword);
+generateBtn.addEventListener('click', function () {
+  // due to scoping issues, the function optionsAll gets called on button click instead of on website load //
+  optionsAll();
+});
 
 
 // //PROMPT \/
